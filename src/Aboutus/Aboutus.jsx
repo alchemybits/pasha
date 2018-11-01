43fadbab0e5f469574bbd86d2a09f0838f8a8530
Armando Roque <roque510@hotmail.com>
2018-11-01 08:48:26 -0600

    Updates About us

--- src/Aboutus/Aboutus.jsx
+++ src/Aboutus/Aboutus.jsx
@@ -15,20 +15,20 @@ function template() {
         <h1 style={{borderBottom: "3pt solid #B68738"}}>About Us</h1>
         <p>Hailing from the enchanting islands of the former Dutch East Indies, &ldquo;PASHA Jewelry for your feet&rdquo; is a beautiful collection of bejeweled sandals that channel an Italian Capri look.</p>
         <p>Each pair is made of highest quality genuine leather and artistically handcrafted in a manner that takes eight to twelve hours per pair, incorporating exclusive Czech rhinestones combined with unique gemstones, shells and natural stones sourced from exotic locations worldwide.</p>
-        <p>These come in flats, wedges and heels, packed with stylish organza bag and a golden shoe box.</p>
-        <p>&ldquo;PASHA Jewelry for your feet&rdquo; can be worn for a day out or for a special evening occasion. They are glitzy, sparkling, glamorous, elegant, stylish and chic with ongoing collections for every season. </p>
+        <p>These beautiful styles come in flats, wedges and heels, further enhanced with a stylish organza bag and a golden shoe box.</p>
+        <p>&ldquo;PASHA Jewelry for your feet&rdquo; can be worn for a day out or for a special evening occasion. They are glitzy, sparkling, glamorous, elegant, stylish and chic with ongoing collections for every season.</p>
         <p>We constantly come up with new styles that are in touch with latest fashion trends, so you will never be bored and always be yearning for more!</p>
         <p>With a thought towards creating wardrobe of bejeweled wear, we have further introduced beachwear, headbands and combination neckwear to compliment &ldquo;Pasha Jewelry for your feet&rdquo;.</p>
-        <p>Our products are carried by retailers who simultaneously carry other luxurious and high-end brands globally. Some of them to name few as follows:</p>
+        <p>Our products are carried by retailers who simultaneously carry other luxurious and high-end brands globally. Some of them to name a few :</p>
         <ul>
-        <li><b>Kitson-Kitross at LA,</b> which is well-known in USA for the shopping paradise of celebrities. One of our style was highlighted on the home page of KITSON's website, and several styles are being sold in its online and physical stores.</li>        
-        <li >Rodeo Boutiques at Atlantis The Palm, Dubai</li>
-        <li >Beach Bunny at La Mer, Dubai</li>
-        <li > <b>Boutiques </b> at Baha Mar, Bahamas</li>
-        <li > <b>Pasha Flagship Stores </b>, Central America</li>        
+        <li><strong>Kitson-Kitross at LA,</strong> which is well-known in USA for the shopping paradise of celebrities. One of our style was highlighted on the home page of KITSON's website, and several styles are being sold in their online and physical stores.</li>
+        <li><strong>Rodeo Boutiques</strong> at Atlantis The Palm, Dubai</li>
+        <li><strong>Beach Bunny</strong> at La Mer, Dubai</li>
+        <li><strong>Boutiques </strong>at Baha Mar, Bahamas</li>
+        <li><strong>Pasha Flagship Stores</strong>, Central America</li>
         </ul>
-        <p>Our quality designs are now featured globally in renowned media, and liked by international figures.</p>
-        <p>Our Corporate Social Responsibility (CSR) extends with PASHA's active participation in O.C.A.D</p>
+        <p>Our quality designs are now featured globally in renowned media and adorned by international figures.</p>
+        <p>PASHA&rsquo;s Corporate Social Responsibility (CSR) extends with an active participation in O.C.A.D</p>
         <br/>
         <div class="flex">
         <div className="wrapper">
@@ -40,7 +40,7 @@ function template() {
           </div>
           <p className="content4p">
           Former First Lady of Honduras Aguas Oca&ntilde;a, <br/> 
-          One of Pasha's Brand Ambassadors.
+          One of Pasha's Brand Ambassadors
           
           </p>
           </div>
@@ -52,8 +52,8 @@ function template() {
             
           </div>
           <p className="content4p">
-          As part of Pasha’s social work in Central America, we support O.C.A.D <br/>
-          foundation towards combating child malnutrition
+          Pasha’s social work in Central America supports O.C.A.D <br/>
+          foundation in combating child malnutrition
           </p>
           </div>
           {/* <p class="" style={{padding:"10px"}}>
